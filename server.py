34bb95a0c4e5bdcef2d817d6f897f4bc317e76a4
Devin <devin.ercolano@gmail.com>
2018-03-10 18:34:56 -0800

    updated password validation to require that pw have at least 1 number and uppercase letter

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import re
 emailRegEx = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-
+passwordCharUppercaseRegEx = re.compile(r'[A-Z]+')
+passwordNumRegEx = re.compile(r'[0-9]+')
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 
@@ -21,7 +22,6 @@ def submit():
     if len(session['firstName']) < 1:
       flash("first name cannot be empty!", 'error')
       error = True
-      return redirect('/')
     elif session['firstName'].isalpha() == False :
       flash("First name must contain only alphabetic characters!", 'error')
       error = True
@@ -36,23 +36,30 @@ def submit():
     if len(session['email']) < 1:
       flash("email cannot be empty!", 'error')
       error = True
-    elif not emailRegEx.match(request.form['email']):
+    elif not emailRegEx.match(session['email']):
       flash("Invalid Email Address!")
       error = True   
 
     if len(session['password']) < 1 :
       flash("Password cannot be empty!", 'error')
       error = True
-    elif len(session['password']) < 8:
+    elif len(session['password']) <= 8:
       flash("Password must be longer than 8 characters")
       error = True
 
-    if len(session['confirmPassword']) < 1 :
-      flash("Password confirmation cannot be empty!", 'error')
-      error = True
-    elif session['password'] != session['confirmPassword'] :
+    if session['password'] != session['confirmPassword'] :
       flash("Password confirmation and password entries must match!", 'error')
       error = True
+
+    m = passwordCharUppercaseRegEx.search(session['password'])
+    if not m :
+      error = True
+      flash("Password must contain at least 1 uppercase letter")
+
+    r = passwordNumRegEx.search(session['password'])
+    if not r :
+      error = True
+      flash("Password must contain at least 1 number")    
 
     if error == True :
       return redirect('/')
